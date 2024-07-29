a4119dfc8f1a8cb5b663f2d0bea76859060dfddb
Mac <mac@MacBook-Pro-Mac.lan>
2024-07-29 20:53:08 +0200

    come one

---
+++ hello/src/typyDanych.java
@@ -0,0 +1,18 @@
+class typyDanych {
+    public static void main(String[] args) {
+        System.out.print("Liczba int: ");
+        System.out.println(25);
+
+        System.out.print("Liczba double: ");
+        System.out.println(25.75);
+
+        System.out.print("Znak A: ");
+        System.out.println('A');
+
+        System.out.print("Czy uczę się programowania? ");
+        System.out.println(true);
+
+        System.out.print("a\n");
+        System.out.print("b\nc");
+    }
+}
---
+++ hello/src/typyDanychcw1.java
@@ -0,0 +1,11 @@
+public class typyDanychcw1 {
+    public static void main(String[] args) {
+        System.out.print("cześć Mateusz, słyszałem, że masz ");
+        System.out.print(25);
+        System.out.println(" lat i uczysz się programowania");
+        System.out.println(true);
+        System.out.println("Czy Twoje inicjały to \tM \tJ?");
+        System.out.print("tak,");
+        System.out.print(true);
+    }
+}
