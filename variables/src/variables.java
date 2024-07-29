1c040b79c424f30f19f847e8829a611e5b7d0c17
Mac <mac@MacBook-Pro-Mac.lan>
2024-07-29 21:55:56 +0200

    oby działało3

--- variables/src/variables.java
+++ variables/src/variables.java
@@ -12,6 +12,10 @@ class variables {
         System.out.println("Poniżej jest kilka informacji o Tobie:");
         System.out.println("Twój PESEL to: " + pesel);
         System.out.println("Masz " + age + " lat");
-        System.out.print("Twój wzrost to: " + height + "cm");
+        System.out.println("Twój wzrost to: " + height + "cm");
+        System.out.println("Jeżeli " + firstName + " urośnie o 5 cm, to " + firstName + " będzie mierzył " + (height + 5));
+
+        firstName = "Gałgan";
+        System.out.println(firstName);
     }
 }
